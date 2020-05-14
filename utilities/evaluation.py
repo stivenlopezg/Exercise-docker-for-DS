de8cd1dd66b96c1d9c09e095cfc669ee957a0fa0
Stiven López Giraldo <stiven.lopez@datalytics.com>
2020-05-14 13:39:53 -0500

    cambios minimos

--- utilities/evaluation.py
+++ utilities/evaluation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics import classification_report, accuracy_score, \
-                            precision_score, recall_score, f1_score, roc_auc_score
+    precision_score, recall_score, f1_score, roc_auc_score
 
 
 def generate_report(y_true: list, y_pred: list):
