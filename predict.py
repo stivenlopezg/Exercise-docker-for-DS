d87a0dbd958199b0cafe2111e057cab0c5314b14
Stiven López Giraldo <stiven.lopez@datalytics.com>
2020-05-14 18:30:28 -0500

    cambios minimos

--- predict.py
+++ predict.py
@@ -1,8 +1,9 @@
 import joblib
 import numpy as np
 import pandas as pd
-from split_data import export_data, load_data, delete_file
+from utilities.config import bucket, key
 from utilities.aws import download_from_s3, upload_to_s3
+from split_data import export_data, load_data, delete_file
 
 
 def load_model(model_path: str):
@@ -31,13 +32,13 @@ def prediction(model, data):
 
 
 def main():
-    download_from_s3()
-    new_data = load_data()
-    model = load_model(model_path='')
+    download_from_s3(bucket=bucket, key=f'{key}/test.csv', dest_pathname='data/test.csv')
+    new_data = load_data(filename='data/test.csv', sep=';')
+    model = load_model(model_path='models/gboosting.pkl')
     result = prediction(model=model, data=new_data)
     export_data(df=result, path='results/churn_users.csv', with_header=True)
-    upload_to_s3()
-    delete_file()
+    upload_to_s3(filename='results/churn_users.csv', bucket=bucket, key=f'{key}/results/churn_users.csv')
+    delete_file(filename='results/churn_user.csv')
     return None
 
 
